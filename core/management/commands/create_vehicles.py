2cbd11a2345bfeec94c670efb6db921c3846129d
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-24 14:23:26 -0300

    feat: Add management commands for vehicle creation and movement simulation, and initial migration for Vehicle model

---
+++ core/management/commands/create_vehicles.py
@@ -0,0 +1,17 @@
+import random
+from django.core.management.base import BaseCommand
+from core.models import Vehicle
+
+class Command(BaseCommand):
+    help = 'Generates vehicle objects in the database'
+
+    def handle(self, *args, **options):
+        NUM_VEHICLES_TO_GENERATE = 5
+        for _ in range(NUM_VEHICLES_TO_GENERATE):
+            # -22.913755052155462, -43.22928637460225
+            Vehicle.objects.create(
+                latitude=random.uniform(-20,-24),
+                longitude=random.uniform(-42,-46)
+            )
+
+        print(f"{Vehicle.objects.count()} vehicles now in the database")

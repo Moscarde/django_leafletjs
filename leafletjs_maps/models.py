01b399fefc4b84ba7b0ddf488a8dc6a013eb892c
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-26 16:53:53 -0300

    feat: set default status for Location model and add route_coordinates view

--- leafletjs_maps/models.py
+++ leafletjs_maps/models.py
@@ -9,6 +9,6 @@ class Location(models.Model):
     address = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default="Aguardando entrega")
     latitude = models.FloatField()
     longitude = models.FloatField()
--- leafletjs_maps/views.py
+++ leafletjs_maps/views.py
@@ -1,6 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from .models import Location, Route
+
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, "index.html", context)
+
+
+def route_coordinates(request):
+    return JsonResponse({"route": list(Route.objects.values("latitude", "longitude"))})
